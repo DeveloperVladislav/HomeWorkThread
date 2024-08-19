1f4f5938f1da34c1362a1bc719a7fbf8d437b788
Vladislav <110498828+DeveloperVladislav@users.noreply.github.com>
2024-08-19 15:40:53 +0300

    Add project files.

---
+++ HomeWorkThread/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace HomeWorkThread
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			// Создаем два потока
+			/*var thread1 = new Thread(() =>
+			{
+				var words = new List<string>() { "П", "р", "и", "в", "е", "т" };
+				foreach (string word in words)
+				{
+					Console.Write(word + " ");
+					Thread.Sleep(200);				
+				}
+				
+			});
+			
+			var thread2 = new Thread(() =>
+			{
+				var words = new List<string>() { "П", "р", "и", "в", "е", "т" };
+				foreach (string word in words)
+				{
+					Console.Write(word + " ");
+					Thread.Sleep(200);
+				}
+			});
+
+			// Запускаем потоки
+			thread1.Start();
+			Thread.Sleep(400);
+			Console.WriteLine();
+			thread2.Start();
+			
+			// Дожидаемся завершения потоков
+			thread1.Join();
+			Console.WriteLine();
+			thread2.Join();
+
+			Console.WriteLine("\nПотоки завершены.");
+			Console.WriteLine();*/
+
+			var shop = new Shop();
+			var random = new Random();
+			var threads = new List<Thread>();
+
+			for (int i = 0; i < 5; i++)
+			{
+				threads.Add(new Thread(() => // создаём нговый поток и добвляем его в список threads
+				{
+					int purchaseCount = random.Next(1, 10);
+
+					shop.MethodSynchronizationLock(purchaseCount);  //способ синхронизации lock
+
+					shop.MethodSynchronizationMonitor(purchaseCount); //способ синхронизации Monitor
+
+					shop.MethodSynchronizationMutex(purchaseCount);  //способ синхронизации Mutex
+
+					shop.MethodSynchronizationSemaphore(purchaseCount);  //способ синхронизации Semaphore
+				}));
+			}
+
+			// Запускаем все потоки
+			foreach (Thread thread in threads)
+			{
+				thread.Start();
+			}
+
+			// Ждем завершения всех потоков
+			foreach (Thread thread in threads)
+			{
+				thread.Join();// блокирует выполнение вызвавшего его потока до тех пор, пока не завершится поток, для которого был вызван данный метод
+			}
+
+			Console.WriteLine($"Всего покупок: {shop.Count}");
+
+		}
+
+	}
+	}
+}
---
+++ HomeWorkThread/Shop.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkThread
+{
+	public class Shop
+	{
+		private object _lock = new();
+		public int Count { get; set; } = 0;
+
+		private static Mutex _mutex = new Mutex();
+
+		private static Semaphore _semaphore = new Semaphore(1, 1);
+
+		public void ShowThread(int amount)
+		{
+			Console.WriteLine($"Группа {Thread.CurrentThread.ManagedThreadId} совершила {amount} покупок. Всего покупок: {Count}");
+		}
+
+		// Метод для увеличения счетчика покупок с использованием lock
+		//Оператор lock определяет блок кода, внутри которого весь код блокируется и становится недоступным для других потоков до завершения работы текущего потока. Остальный потоки помещаются в очередь ожидания и ждут, пока текущий поток не освободит данный блок кода.
+		public void MethodSynchronizationLock(int amount)
+		{
+			lock (_lock)
+			{
+				Count += amount;
+				ShowThread(amount);
+			}
+		}
+
+		// Метод для увеличения счетчика покупок с использованием Monitor
+		public void MethodSynchronizationMonitor(int amount)
+		{
+			Monitor.Enter(_lock);//принимает объект блокировки
+			try
+			{
+				Count += amount;
+				ShowThread(amount);
+			}
+			finally
+			{
+				Monitor.Exit(_lock); //происходит освобождение объекта locker, если блокировка осуществлена успешно, и он становится доступным для других потоков
+			}
+		}
+
+		// Метод для увеличения счетчика покупок с использованием Mutex	
+		/*Mutex предназначен для синхронизации между процессами, а не потоками в рамках одного процесса.*/
+		public void MethodSynchronizationMutex(int amount)
+		{
+			_mutex.WaitOne();
+			try
+			{
+				Count += amount;
+				ShowThread(amount);
+			}
+			finally
+			{
+				_mutex.ReleaseMutex();
+			}
+		}
+
+		// Метод для увеличения счетчика покупок с использованием Semaphore
+		//Семафоры позволяют ограничить количество потоков, которые имеют доступ к определенным ресурсам.
+		public void MethodSynchronizationSemaphore(int amount)
+		{
+			_semaphore.WaitOne(); //ожидает получения свободного места в семафоре
+			try
+			{
+				Count += amount;
+				ShowThread(amount);
+			}
+			finally
+			{
+				_semaphore.Release();//освобождает место в семафоре
+			}
+		}
+	}
+}
